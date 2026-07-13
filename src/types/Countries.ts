d450e570a8e155185364d8e7745b888bb92ca90a
Wietse Verkade <simpelcity@users.noreply.github.com>
2026-07-13 22:14:08 +0200

    fix

--- src/types/Countries.ts
+++ src/types/Countries.ts
@@ -1,6 +1,6 @@
 export type CountriesUSA = "USA" | "Iran" | "Turkey" | "Australia" | "Greece" | "Canada" | "Republic of Vietnam" | "Norway" | "Philippines" | "China" | "Great Britain" | "Israel";
-export type CountriesGermany = "Germany" | "Poland" | "GDR" | "Canada (modern)" | "Lithuania" | "German Empire" | "Spain" | "Finland" | "FRG" | "Romania" | "South Africa" | "Switzerland" | "Argentina";
-export type CountriesUSSR = "USSR" | "Cuba" | "Venezuela" | "Russia" | "Syria" | "Slovakia" | "Russian Empire" | "Czech Republic" | "Serbia" | "Kazakhstan";
+export type CountriesGermany = "Germany" | "Poland" | "GDR" | "Canada (modern)" | "Lithuania" | "German Empire" | "Spain" | "Finland" | "FRG" | "Romania" | "South Africa (modern)" | "Switzerland" | "Argentina";
+export type CountriesUSSR = "USSR" | "Cuba" | "Venezuela" | "Russia" | "Syria" | "Slovakia" | "Russian Empire" | "Czech Republic" | "Serbia" | "Kazakhstan" | "China";
 export type CountriesGreatBritain = "Great Britain" | "South Africa (modern)" | "India" | "Canada (modern)" | "Jordan" | "South Africa" | "Canada" | "New Zealand" | "Ireland" | "Poland" | "Australia" | "Kuwait";
 export type CountriesJapan = "Japan" | "Indonesia" | "Malaysia" | "Thailand";
 export type CountriesChina = "China" | "Pakistan" | "Vietnam" | "North Korea" | "Bangladesh" | "Republic of China";
