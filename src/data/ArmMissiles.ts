c2e2ea7ffc115b978e8a3a3cf7364994b93087de
Simpelcity <wietse2007.3@gmail.com>
2026-09-09 16:54:42 +0200

    test

--- src/data/ArmMissiles.ts
+++ src/data/ArmMissiles.ts
@@ -748,7 +748,7 @@ export const armMissiles: ArmDefinition[] = [
         vehicleRank: "VII",
         vehicleBr: {
           AB: "11.3",
-          RB: "11.0",
+          RB: "11.3",
           SB: "11.7"
         }
       }
